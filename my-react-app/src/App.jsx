7dfcaacc3ea97472d98e1bd488306a2cf0829ef1
Joachim Christiansen <joachilc@hiof.no>
2025-04-16 12:11:15 +0200

    added layout to be used

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,19 @@
 import './styles.scss';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MemberPage from './pages/MemberPage';
 import FrontPage from './pages/FrontPage';
 import Layout from './components/Layout';
 
 function App() {
   return (
     <Router>
+      <Layout>
         <Routes>
           <Route path="/" element={<FrontPage />} />
+          <Route path="/member/:id" element={<MemberPage />} />
         </Routes>
+      </Layout>
     </Router>
   );
 }
